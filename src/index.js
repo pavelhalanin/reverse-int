25f9e7d58f9d6a19f415afc3f742c29a15c41dfb
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-04-08 22:25:11 +0300

    feat: test is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+/**
+ * How to run test:
+ * 1) Install NodeJS
+ * 2) Instal node_modules on repo: `npm i`
+ * 3) Start test: `npm run test`
+ */
+
+module.exports = function reverse(n) {
+  return Number(`${Math.abs(n)}`.split('').reverse().join(''));
 };
